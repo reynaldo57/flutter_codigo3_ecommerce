18072bfeb41f239832de20e36478994dd047a3c6
reynaldo57 <reynaldo57@hotmail.com>
2022-04-16 13:01:27 -0500

    cartPage 3

--- lib/page/cart_page.dart
+++ lib/page/cart_page.dart
@@ -1,8 +1,6 @@
-
 import 'package:flutter/material.dart';
 
 class CartPage extends StatefulWidget {
-
   @override
   _CartPageState createState() => _CartPageState();
 }
@@ -11,6 +9,7 @@ class _CartPageState extends State<CartPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         elevation: 0,
         backgroundColor: Colors.white,
@@ -27,49 +26,99 @@ class _CartPageState extends State<CartPage> {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Container(
-                  width: 100,
-                  height: 100,
-                  margin: EdgeInsets.only(right: 0.0),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(14.0),
-                    image: DecorationImage(
-                      fit: BoxFit.cover,
-                      image: NetworkImage("http://192.168.18.6:8000/media/banners/banner_1.jpg"),
-                    )
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 6.0, vertical: 7.0),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(14.0),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black12.withOpacity(0.07),
+                    blurRadius: 10,
+                    offset: Offset(0,4),
+                  )
+                ]
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    width: 100,
+                    height: 100,
+                    margin: EdgeInsets.only(right: 6.0),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(14.0),
+                        image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: NetworkImage(
+                              "http://192.168.18.6:8000/media/banners/banner_1.jpg"),
+                        )),
                   ),
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text("Adidas", style: TextStyle(fontSize: 12.0),),
-                    Text("Zapatillas SuperCourt", style: TextStyle(fontSize: 13.0, fontWeight: FontWeight.bold),),
-                    Text("S/ 299", style: TextStyle(fontSize: 12.0),),
-                    SizedBox(
-                      height: 4.0,
+                  Flexible(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Adidas",
+                          style: TextStyle(fontSize: 12.0),
+                        ),
+                        Text(
+                          "Zapatillas SuperCourt SuperCourt SuperCourt",
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(
+                              fontSize: 13.0, fontWeight: FontWeight.bold,),
+                        ),
+                        Text(
+                          "S/ 299",
+                          style: TextStyle(fontSize: 12.0),
+                        ),
+                        SizedBox(
+                          height: 4.0,
+                        ),
+                        Text(
+                          "Remover",
+                          style: TextStyle(
+                              fontSize: 12.0,
+                              decoration: TextDecoration.underline),
+                        ),
+                      ],
                     ),
-                    Text("Remover", style: TextStyle(fontSize: 12.0, decoration: TextDecoration.underline),),
-                  ],
-                ),
-                Column(
-                  children: [
-                    Text("Cant. ", style: TextStyle(fontSize: 12.0),),
-                    Text("2", style: TextStyle(fontSize: 13.0, fontWeight: FontWeight.bold),),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: Column(
+                      children: [
+                        Text(
+                          "Cant. ",
+                          style: TextStyle(fontSize: 12.0),
+                        ),
+                        Text(
+                          "2",
+                          style: TextStyle(
+                              fontSize: 12.0, fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Column(
+                    children: [
+                      Text(
+                        "Total",
+                        style: TextStyle(fontSize: 12.0),
+                      ),
+                      Text(
+                        "S/ 500",
+                        style: TextStyle(
+                            fontSize: 13.0, fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
 
-                  ],
-                ),
-                Column(
-                  children: [
-                    Text("Total", style: TextStyle(fontSize: 12.0),),
-                    Text("2", style: TextStyle(fontSize: 13.0, fontWeight: FontWeight.bold),),
 
-                  ],
-                ),
-              ],
-            )
           ],
         ),
       ),
